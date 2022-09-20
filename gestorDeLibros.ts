2d8995660b22c280f00678a74bab9a10b560dc24
Emiliano Gonzalez <emilianogonzalez@tdf.edu.ar>
2022-09-20 00:06:03 -0300

    initial commit

---
+++ gestorDeLibros.ts
@@ -0,0 +1,21 @@
+import libro from "./libro";
+import  stockLibros  from "./libro";
+
+class gestorDeLibros{
+    private nombre : string;
+    private libros : typeof stockLibros  [] 
+
+  public getLibro (){
+
+   }
+   public insertLibro (){
+
+   }
+   private eliminarLibro (){
+
+   }
+   public modificarLibro (){
+
+   }
+}
+console.log(stockLibros)
